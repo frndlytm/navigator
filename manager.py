a33c66bc60750a0f11c34882aeca9f059099e836
Christian DiMare <frndlytm@gmail.com>
2018-05-14 11:28:45 -0400

    Removed relative imports from the module.

--- manager.py
+++ manager.py
@@ -4,12 +4,15 @@
 @description:
     A manager consumes all of the factories and builds their results on concrete metadata.
 """
-from .paginator import Paginator
+from paginator import Paginator
 
 class Manager:
     def __init__(self):
         self.sources = []
-        self.setup()
+        self._session = None
+        self._factory = None
+        self._paginator = None
+        self._docs = None
 
     @property
     def session(self):
@@ -26,13 +29,13 @@ class Manager:
         self._factory = f
 
     @property
-    def meta(self):
-        return self._meta
-    @meta.setter
-    def meta(self, m):
-        assert isinstance(m, tuple)
-        assert len(m) == 2
-        self._meta = m
+    def docs(self):
+        return self._docs
+    @docs.setter
+    def docs(self, d):
+        assert isinstance(d, tuple)
+        assert len(d) == 2
+        self._docs = d
 
     @property
     def paginator(self):
@@ -46,11 +49,11 @@ class Manager:
 
     def setup(self):
         # Add the source for endpoint lookup.
-        src = self.factory.make(None, self.meta[0])
+        src = self.factory.make(None, self.docs[0])
         self.add_source(src)
 
         # For endpoint in the endpoint lookup, add the source.
-        for record in self.sources[0].get(s=self.session)[self.meta[1]]:
+        for record in self.sources[0].get(s=self.session)[self.docs[1]]:
             path = record['path'][1:].split('/')
             version = path[0]
             endpoint = path[1]
@@ -60,5 +63,6 @@ class Manager:
 
     def add_source(self, src):
         self.sources.append(src)
+
 
 
